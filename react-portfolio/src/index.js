077392b8a678a481981c9ead15896fdfaf231480
jaburgess <jaburgess@uiowa.edu>
2022-05-22 18:43:39 -0500

    Added home page,letter animations,side bar and links

---
+++ react-portfolio/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+//Wraping the application inside the router alows us to use the 'routs' inside out web app.
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
